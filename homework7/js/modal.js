c62a7cca307502b2dff37e9bb447ac8663c828f0
Torobaev23 <150238405+Torobaev23@users.noreply.github.com>
2024-01-29 11:30:54 +0300

    Add files via upload

---
+++ homework7/js/modal.js
@@ -0,0 +1,48 @@
+// MODAL
+
+const modal = document.querySelector('.modal')
+const modalTriggerButton = document.querySelector('#btn-get')
+const closeModalButton = document.querySelector('.modal_close')
+
+const openModal = () => {
+    modal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+
+}
+
+modalTriggerButton.onclick = () => {
+    openModal()
+}
+
+const closeModal = () => {
+    modal.style.display = 'none'
+    document.body.style.overflow = ''
+    window.removeEventListener("scroll", scrollTop);
+}
+
+closeModalButton.onclick = () => {
+    closeModal()
+}
+
+modal.onclick = (event) => {
+    if (event.target === modal ) {
+        closeModal()
+    }
+}
+
+setTimeout(() => {
+    openModal()
+}, 10000 )
+
+
+
+const scrollTop = () => {
+    if(window.scrollY+1 >= document.documentElement.scrollHeight-document.documentElement.clientHeight)  {
+        openModal();
+    }
+}
+
+// window.addEventListener("scroll", scrollTop);
+
+
+
